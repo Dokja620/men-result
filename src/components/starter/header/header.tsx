82cfec32c376ba2e89b5985749274f3db88f70f3
HomeJa <revanamike620@gmail.com>
2024-07-23 17:17:23 +0300

    🐞 fix(images💅🖼️):

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,15 +1,13 @@
 
 import { component$ } from "@builder.io/qwik";
-import ImgMen from '~/media/media/men.webp?jsx';
-import ImgMada from '~/media/media/mada.webp?jsx';
 
 export default component$(() => {
     return (
         <header>
             <nav>
-                <a href="/" class="mada"><span><ImgMada alt="MADA logo" /></span></a>
+                <a href="/" class="mada"><span></span></a>
                 <p>&</p>
-                <a href="/" class="men"><span><ImgMen alt="MEN logo" /></span></a>
+                <a href="/" class="men"><span></span></a>
             </nav>
         </header>
     );
