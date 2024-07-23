665b9c95a5ab24d1f0b93d56883c4395ce1effe3
HomeJa <revanamike620@gmail.com>
2024-07-23 16:50:55 +0300

    fixes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Insight } from "./sections/insight";
-import { Search } from "./sections/search";
-import { List } from "./sections/list";
+// import { Search } from "./sections/search";
+// import { List } from "./sections/list";
 import { Hero } from "./sections/hero";
 
 export default component$(() => {
